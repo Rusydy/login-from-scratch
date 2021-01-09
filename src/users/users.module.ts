76069ffb71fb0eb3a60f7831a1797491e2bf91c7
Rusydy <rusydymuhiddin@gmail.com>
2021-01-09 12:26:34 +0700

    login success

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,7 +5,7 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{name: 'loginTest', schema: UserSchema}])],
+  imports: [MongooseModule.forFeature([{name: 'User', schema: UserSchema}])],
   controllers: [UsersController],
   providers: [UsersService]
 })
